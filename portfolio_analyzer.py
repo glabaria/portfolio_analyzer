54cb48af222a9b174eec6c56e4a973d3a6db1963
glabaria <george.labaria@gmail.com>
2022-05-06 15:47:09 -0700

    -enhance Function doc-strings

--- portfolio_analyzer.py
+++ portfolio_analyzer.py
@@ -12,12 +12,19 @@ IGNORE_LIST = ['INTRA-ACCOUNT TRANSFER']
 
 class PortfolioAnalyzer:
 
-    def __init__(self, transaction_csv_path=None, save_file_path=None):
+    def __init__(self, transaction_csv_path=None, save_file_path=None, benchmark_ticker_list=('dia', 'spy', 'qqq')):
         self.transaction_csv_path = transaction_csv_path
         self.save_file_path = save_file_path
+        self.benchmark_ticker_list = benchmark_ticker_list
         sns.set()
 
     def gather_data(self):
+        """
+        Helper function to to gather transaction and account balance csvs
+
+        :return:
+            Return tuple (transaction_df, portfolio_value_df)
+        """
 
         # read transaction CSV data
         filename_list = os.listdir(self.transaction_csv_path)
@@ -35,6 +42,14 @@ class PortfolioAnalyzer:
 
     @staticmethod
     def calculate_daily_returns(transaction_df, portfolio_value_df):
+        """
+        Calculate daily returns from TD Ameritrade transaction and portfolio Dataframes
+
+        :param transaction_df: Dataframe consisting of the user's transactions
+        :param portfolio_value_df: Dataframe consisting of the portfolio's balence
+        :return:
+            Dataframe consisting of portfolio daily and cumulative returns
+        """
         # get dates and amount of any account deposits
         transaction_df['IS_DEPOSIT'] = transaction_df['DESCRIPTION'].apply(lambda x: x in DEPOSIT_DESCRIPTION_LIST)
 
@@ -64,7 +79,7 @@ class PortfolioAnalyzer:
             ['Account value', 'ACCOUNT_VALUE_EX_DEPOSIT']] = \
             portfolio_value_unique_date_with_deposit[['Account value', 'ACCOUNT_VALUE_EX_DEPOSIT']].values
         portfolio_value_unique_date['daily_return_pct'] = \
-            np.ones_like(portfolio_value_unique_date.iloc[:, 1].values) * np.nan
+            np.zeros_like(portfolio_value_unique_date.iloc[:, 1].values)
         portfolio_value_unique_date.loc[1:, 'daily_return_pct'] = \
             np.round((portfolio_value_unique_date.iloc[1:, -2].values /
                       portfolio_value_unique_date.iloc[:-1, -3].values - 1) * 100, 2)
@@ -77,13 +92,22 @@ class PortfolioAnalyzer:
 
     @staticmethod
     def calculate_daily_return_from_ticker(ticker_list, start_date, end_date):
+        """
+        Calculate daily returns from a ticker list
+
+        :param ticker_list: List of symbols to calculate returns for
+        :param start_date: Start date of query
+        :param end_date: End date of query
+        :return: yfinance Dataframe of ticker data
+        """
+
         yf_df = yf.download(ticker_list, start=start_date, end=end_date, interval='1d')
         ind_array = np.array([(i, x[-1]) for i, x in enumerate(yf_df.columns.values) if x[0] == 'Adj Close'])
         ind_array, ticker_list = np.array([int(x[0]) for x in ind_array]), [x[1] for x in ind_array]
 
         # calculate daily return
         col_array = [('daily_return_pct', x) for x in ticker_list]
-        yf_df[col_array] = np.ones_like(yf_df.iloc[:, ind_array].values) * np.nan
+        yf_df[col_array] = np.zeros_like(yf_df.iloc[:, ind_array].values)
         yf_df.iloc[1:, -3:] = np.round((yf_df.iloc[1:, ind_array].values / yf_df.iloc[:-1, :3].values - 1) * 100, 2)
 
         # calculate cumulative return
@@ -92,12 +116,17 @@ class PortfolioAnalyzer:
 
         return yf_df, ticker_list
 
-    @staticmethod
-    def convert_date_to_yfinance_format(dates):
-        return np.array(['-'.join(np.array(x.split('/'))[np.array([2, 0, 1])]) for x in dates])
-
     def plot_return_pct(self, portfolio_df, benchmark_df, title='Portfolio performance since inception',
                         file_name='portfolio_performance'):
+        """
+        Function to plot portfolio return pct
+
+        :param portfolio_df:
+        :param benchmark_df:
+        :param title:
+        :param file_name:
+        :return:
+        """
 
         plt.figure(figsize=(14, 8))
         plt.plot(portfolio_df.cumulative_return_pct.values, label='Portfolio', linewidth=2)
@@ -119,6 +148,14 @@ class PortfolioAnalyzer:
 
     @staticmethod
     def ensure_equal_dates(df1, df2):
+        """
+        Utility function to ensure that df1 and df2 have the same dates.  Dates that are not in the intersection of
+        df1.Date and df2.Date are discarded.
+
+        :param df1: First Dataframe (note: must contain a column called "Date")
+        :param df2: Second Dataframe (note: must contain a column called "Date")
+        :return: Tuple (df1, df2)
+        """
         date1 = set(list(df1.Date.values))
         date2 = set(list(df2.Date.values))
         date = date1.intersection(date2)
@@ -129,6 +166,19 @@ class PortfolioAnalyzer:
     @staticmethod
     def calculate_cumulative_returns_from_date(df, start_date, daily_return_column_name='daily_return_pct',
                                                cum_return_column_name=None):
+        """
+        This function calculates the cumulative return % from the daily return %.
+
+        :param df: Dataframe containing the daily_return_column_name
+        :param start_date: Pandas datetime to start calculating the cumulative daily return from
+        :param daily_return_column_name: Name of the column that contains the daily return %
+        :param cum_return_column_name: Name of the column to store the cumulative return calculated (can also be tuple
+            for multi-indexed columns)
+        :return:
+            Copy of dataframe df that contains the cum_return_column_name of cumulative returns starting from
+            start_date.  Note that the dates in this Dataframe are truncated to only include start_date onwards
+        """
+
         df = df.copy()
         df = df.loc[df.Date >= start_date]
         if cum_return_column_name is None:
@@ -140,9 +190,8 @@ class PortfolioAnalyzer:
         transaction_df, portfolio_value_df = self.gather_data()
         portfolio_df = self.calculate_daily_returns(transaction_df, portfolio_value_df)
         dates = portfolio_df.Date.dt.strftime('%Y-%m-%d').values
-        ticker_list = ['dia', 'spy', 'qqq']
         benchmark_df, ticker_list = \
-            self.calculate_daily_return_from_ticker(ticker_list, start_date=dates[0], end_date=dates[-1])
+            self.calculate_daily_return_from_ticker(self.benchmark_ticker_list, start_date=dates[0], end_date=dates[-1])
         benchmark_df.reset_index(inplace=True)
         portfolio_df, benchmark_df = self.ensure_equal_dates(portfolio_df, benchmark_df)
 
@@ -171,7 +220,9 @@ class PortfolioAnalyzer:
 def run_portfolio_analyzer():
     transaction_csv_path = './transaction_data/'
     save_file_path = './figures/'
-    PortfolioAnalyzer(transaction_csv_path=transaction_csv_path, save_file_path=save_file_path).run()
+    benchmark_ticker_list = ['dia', 'spy', 'qqq']
+    PortfolioAnalyzer(transaction_csv_path=transaction_csv_path, save_file_path=save_file_path,
+                      benchmark_ticker_list=benchmark_ticker_list).run()
 
 
 if __name__ == '__main__':
