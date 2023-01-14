5f8aa28078bcf8082bb63c88b48cb8cb55272f69
glabaria <george.labaria@gmail.com>
2023-01-14 13:08:04 -0800

    -enhance output_dir arguments

--- portfolio_analyzer_tool/cli.py
+++ portfolio_analyzer_tool/cli.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import numpy as np
 from portfolio_analyzer_tool.portfolio_analyzer import PortfolioAnalyzer
 
@@ -8,7 +9,7 @@ def cli():
     parser.add_argument("-i", "--input_dir", type=str,
                         help="Path to directory which contains the 'chart.csv' and 'transactions_{year}.csv' files")
     parser.add_argument("-o", "--output_dir", type=str,
-                        help="Path to directory to save figures")
+                        help="Path to directory to save results.  Defaults to current working directory.")
     parser.add_argument("-b", "--benchmark", action="store_true", help="Flag to benchmark portfolio")
     parser.add_argument("--tickers", type=str,
                         help="Ticker list to compare to separated by commas; e.g. 'spy,qqq'")
@@ -27,13 +28,15 @@ def cli():
 
     args = parser.parse_args()
 
+    output_dir = args.output_dir if args.output_dir is not None else os.getcwd()
+
     # get ticket list to compare against
     ticker_list = ['dia', 'spy', 'qqq', 'vt'] if args.tickers is None else args.tickers.split(",")
 
     # get days to compare
     from_da_list = [30, 90, 180, 365, 730, 'ytd', -np.inf] if args.from_da is None else args.from_da.split(",")
 
-    pa_obj = PortfolioAnalyzer(transaction_csv_path=args.input_dir, save_file_path=args.output_dir,
+    pa_obj = PortfolioAnalyzer(transaction_csv_path=args.input_dir, save_file_path=output_dir,
                                benchmark_ticker_list=ticker_list, benchmark_startdate_list=from_da_list,
                                sharp_ratio=args.sharpe_ratio, sliding_corr=args.sliding_corr)
     pa_obj.run()
